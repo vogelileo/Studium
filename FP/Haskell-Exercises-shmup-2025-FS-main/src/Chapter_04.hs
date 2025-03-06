d812fc9e4082f57cffb7aff41239c27442be537f
Leo Vogel <Leo.Vogel@ibm.com>
2025-03-06 09:02:22 +0100

    haskell 4/5

--- FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_04.hs
+++ FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_04.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Collapse lambdas" #-}
 {-# HLINT ignore "Redundant lambda" #-}
 module Chapter_04 where
@@ -7,34 +8,36 @@ module Chapter_04 where
 
 -- Exercise 4.3 (**)
 -- Consider a function `safeTail :: [a] -> [a]` that behaves in the same way as tail except that it maps the empty list to itself rather than producing an error. Using `tail` and the function `null :: [a] -> Bool` that decides if a list is empty or not, define safetail using:
--- (a) a conditional expression; 
+-- (a) a conditional expression;
 -- (b) guarded equations;
 -- (c) pattern matching.
 
 safeTailA :: [a] -> [a]
--- safeTailA xs = if ??? then ??? else ???
-safeTailA = undefined
+-- safeTailA xs = if null then [] else xs
+safeTailA xs = if null xs then [] else xs
 
 safeTailB :: [a] -> [a]
--- safeTailB xs | ??? = ???
---              | ??? = ???
-safeTailB = undefined
+-- safeTailB xs | null = []
+--              | otherwise = xs
+safeTailB xs
+  | null xs = []
+  | otherwise = tail xs
 
 safeTailC :: [a] -> [a]
--- safeTailC ??? = ???  
 -- safeTailC ??? = ???
-safeTailC = undefined
-
-
-
+-- safeTailC ??? = ???
+safeTailC [] = []
+safeTailC (_ : xs) = xs
 
 -- Exercise 4.7 (**)
 -- Show how the meaning of the following curried function definition can be formalized in terms of lambda expressions:
---     mult :: Int -> Int -> Int -> Int 
+--     mult :: Int -> Int -> Int -> Int
 --     mult x y z = x*y*z
 
-mult :: Int -> (Int -> (Int -> Int)) 
+mult :: Int -> (Int -> (Int -> Int))
 -- mult = ???
-mult = undefined
+mult = \x -> (\y -> (\z -> (x * y * z)))
 
+-- >>> mult 2 3 4
 
+-- 24
--- FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_05.hs
+++ FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_05.hs
@@ -4,23 +4,30 @@ module Chapter_05 where
 -- Using a list comprehension, give an expression that calculates the sum 1^2 + 2^2 + . . . + 100^2 of the first one hundred integer squares. You may use the function `sum :: [Int] -> Int` in your definition.
 
 e5_1 :: Int
-e5_1 = sum [ undefined | undefined ]
+e5_1 = sum [x ^ 2 | x <- [1 .. 100]]
 
+-- >>> e5_1
+-- 338350
 
 -- Exercise 5.4 (*)
--- In a similar way to the function `length` defined as follows, 
+-- In a similar way to the function `length` defined as follows,
 --     length ::[a] -> Int
 --     length xs = sum [1 | _ <- xs]
--- show how the library function `replicate :: Int -> a -> [a]` 
+-- show how the library function `replicate :: Int -> a -> [a]`
 -- that produces a list of identical elements can be defined using a list comprehension.
 -- For example:
---     > replicate 3 True 
+--     > replicate 3 True
 --     [True,True,True]
 -- Note: If you are trying this within ghc, please note that replicate is already defined in the Prelude. Use the name myReplicate which is currently undefined instead.
 
 myReplicate :: Int -> a -> [a]
-myReplicate n x = [ undefined | undefined ]
+myReplicate n x = [x | n <- [1 .. n]]
 
+-- >>> myReplicate 3 True
+-- [True,True,True]
+
+-- >>> myReplicate 15 "abc"
+-- ["abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc"]
 
 -- Exercise 5.8 (**)
 -- Redefine the function `positions`
@@ -29,14 +36,27 @@ myReplicate n x = [ undefined | undefined ]
 -- without using list comprehension, but using the functions find and zip.
 
 -- `zip` is provided by the Standard Prelude:
---     zip :: [a] -> [b] -> [(a,b)] 
+--     zip :: [a] -> [b] -> [(a,b)]
 --     zip [] _ = []
 --     zip _ [] = []
 --     zip (a:as) (b:bs) = (a,b) : zip as bs
 
-find :: Eq a => a -> [(a,b)] -> [b]
-find k t = [v | (k',v) <- t, k == k']
+find :: (Eq a) => a -> [(a, b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
 
-positions :: Eq a => a -> [a] -> [Int]
-positions x xs = undefined
+positions :: (Eq a) => a -> [a] -> [Int]
+positions x xs = find x (zip xs [0 .. length xs])
 
+-- >>> zip [1,2,3,4] [0..length [1,2,3,4]]
+-- [(1,0),(2,1),(3,2),(4,3)]
+
+-- >>> positions 3 [1, 2, 3, 4, 3, 5] -- Expected: [2, 4]
+-- >>> positions 'a' "banana" -- Expected: [1, 3, 5]
+-- >>> positions 42 [1, 2, 3, 4, 5] -- Expected: []
+-- >>> positions 'x' "" -- Expected: []
+-- >>> positions 5 [5, 5, 5, 5, 5] -- Expected: [0,1,2,3,4]
+-- [2,4]
+-- [1,3,5]
+-- []
+-- []
+-- [0,1,2,3,4]
