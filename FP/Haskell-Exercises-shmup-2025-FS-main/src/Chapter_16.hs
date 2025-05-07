fdf6793b2e4e886f0ead1c98d3b86bb7830914f9
Leo Vogel <Leo.Vogel@ibm.com>
2025-05-07 10:51:34 +0200

    added Chapter 16

--- FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_16.hs
+++ FP/Haskell-Exercises-shmup-2025-FS-main/src/Chapter_16.hs
@@ -2,12 +2,11 @@ module Chapter_16 where
 
 -- Note: There are neither any tests nor automated tests for this chapter. It appears in this form in order to have a uniform format with respect to the previous chapters.
 
-
 data Nat = Zero | Succ Nat
 
 add :: Nat -> Nat -> Nat
-add Zero m = m                      -- add_Zero
-add (Succ n) m = Succ (add n m)     -- add_Succ
+add Zero m = m -- add_Zero
+add (Succ n) m = Succ (add n m) -- add_Succ
 
 -- Exercise 16.1
 -- Show that `add n (Succ m) == Succ (add n m)`, by induction on `n`.
@@ -17,18 +16,33 @@ add (Succ n) m = Succ (add n m)     -- add_Succ
 -- ∀m.   add Zero m == m                      -- add_Zero
 -- ∀n m. add (Succ n) m == Succ (add n m)     -- add_Succ
 -- ∀x.   (x == x) == True                     -- (==)_refl
---
+
 -- 2. When using the above properties, you are required to refer to them explicitly using the names as shown above. You are required to state and justify each step of the proof explicitly and only apply a single rule (equational rewriting or otherwise) at each step of the proof. In case you use equational reasoning, please underline the sub terms that you rewrite at each step. In case you use induction, please explicitly state the variable upon which you choose to perform induction.
 
 -- Hint: Fist state the induction principle that follows from the definition of `Nat`, and then instantiate it to get the base case and the inductive case statements that are required to prove the statement.
 
 -- Specify your answer within the following block comment
 {-
-???
+
+Base case
+add 0 (Succ m) == Succ (add 0 m) //by using add_Zero
+
+Hypthesis
+add n (Succ m) == Succ (add n m)
+
+to show:
+add (Succ n) (Succ m) == Succ (add (Succ n) m)
+
+left side:
+add (Succ n) (Succ m) -> Succ (add n (Succ m)) //by using add_Succ
+Succ (add n (Succ m)) -> Succ (Succ (add n m)) //by using add_Succ
+
+right side:
+Succ (add (Succ n) m) -> Succ (Succ (add n m)) //by using add_Succ
+
+Now left and right side are same // by using (==)_refl
+
 -}
-
-
-
 
 -- Exercise 16.2
 -- Using the property `add n (Succ m) = Succ (add n m)`, together with `add n Zero = n`, show that addition is commutative (i.e. `add n m = add m n`) , by induction on `n`.
@@ -45,6 +59,29 @@ add (Succ n) m = Succ (add n m)     -- add_Succ
 
 -- Specify your answer within the following block comment
 {-
-???
--}
+add n m = add m n
 
+Base case
+
+add Zero m = m // add_Zero
+add m Zero = m // add_Zero_flipped
+m == m // (==)_refl
+
+Hypothesis
+add n m == add m n
+
+to show:
+add n (Succ m) == add (Succ m) n
+
+left side:
+Succ (add n m) //add_Suc
+
+right side:
+Succ (add m n) //addSucc
+
+complete sides:
+Succ (add n m) == Succ (add m n) -> Succ (add m n) == Succ (add m n) //using Hypothesis
+
+Succ (add m n) == Succ (add m n) //(==) _refl
+
+-}
